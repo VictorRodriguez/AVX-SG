8707b10a852454789b9b696a25e4c686c3a43c0e
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-08-25 21:15:18 +0000

    check operations basic_arit
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- basic_arit_asm.c
+++ basic_arit_asm.c
@@ -11,8 +11,8 @@ int main() {
 
     int i,j;
 
-    for ( i = 0 ; i < 1000000000 ; i++){
-    for ( j = 0 ; j < 1000000000 ; j++){
+    for ( i = 0 ; i < 100 ; i++){
+    for ( j = 0 ; j < 100 ; j++){
     /* Perform Addition, Subtraction, Multiplication & Division */
     __asm__ ( "addl %%ebx, %%eax;" : "=a" (add) : "a" (arg1) , "b" (arg2) );
     __asm__ ( "subl %%ebx, %%eax;" : "=a" (sub) : "a" (arg1) , "b" (arg2) );
@@ -23,7 +23,18 @@ int main() {
               "movl %3, %%ebx;"
                "idivl %%ebx;" : "=a" (quo), "=d" (rem) : "g" (arg1), "g" (arg2) );
     }}
-
+/*
+    if (arg1 + arg1 != add)
+        printf("FAIL\n"); return -1;
+    if (arg1 - arg2 != sub)
+        printf("FAIL\n"); return -1;
+    if (arg1 * arg1 != mul)
+        printf("FAIL\n"); return -1;
+    if (arg1 / arg1 != quo)
+        printf("FAIL\n"); return -1;
+    if (arg1 % arg1 != rem)
+        printf("FAIL\n"); return -1;
+*/
     printf( "%d + %d = %d\n", arg1, arg2, add );
     printf( "%d - %d = %d\n", arg1, arg2, sub );
     printf( "%d * %d = %d\n", arg1, arg2, mul );
