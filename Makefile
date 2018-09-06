6c5312e12700679f35e67754386fec274b7be147
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-09-06 15:17:27 +0000

    firx for vnni
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ all:
 	gcc -O3 -march=skylake-avx512 $(src_dir)stress_add_d_avx512.c -o $(build_dir)stress_add_d_avx512
 	gcc -O3 -march=skylake-avx512 $(src_dir)stress_add_i_avx512.c -o $(build_dir)stress_add_i_avx512
 	gcc -O3 -march=skylake-avx512 -mavx5124vnniw $(src_dir)4dpwssd_epi32.c -o $(build_dir)4dpwssd_epi32
-	gcc -O3 -march=icelake-server $(src_dir)dpbusd_epi32.c -o $(build_dir)dpbusd_epi32
+	gcc -O3 -march=icelake-server $(src_dir)stress_dpbusd_vnni.c -o $(build_dir)stress_dpbusd_vnni
 
 static:
 	mkdir -p $(build_dir)
@@ -34,7 +34,7 @@ static:
 	gcc -O3 -march=skylake-avx512 $(src_dir)stress_add_d_avx512.c -o $(build_dir)stress_add_d_avx512 $(static_flags)
 	gcc -O3 -march=skylake-avx512 $(src_dir)stress_add_i_avx512.c -o $(build_dir)stress_add_i_avx512 $(static_flags)
 	gcc -O3 -march=skylake-avx512 -mavx5124vnniw $(src_dir)4dpwssd_epi32.c -o $(build_dir)4dpwssd_epi32 $(static_flags)
-	gcc -O3 -march=icelake-server $(src_dir)dpbusd_epi32.c -o $(build_dir)dpbusd_epi32 $(static_flags)
+	gcc -O3 -march=icelake-server $(src_dir)stress_dpbusd_vnni.c -o $(build_dir)stress_dpbusd_vnni $(static_flags)
 
 check: all
 	./$(build_dir)basic_add_asm
---
+++ src/stress_dpbusd_vnni.c
@@ -0,0 +1,88 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <immintrin.h>
+#include "common.h"
+
+#define BILLION 1E9
+
+void foo(){
+
+    __m128i values = _mm_setr_epi32(0x1234, 0x2345, 0x3456, 0x4567);
+    values = _mm_dpbusd_epi32(values, values, values);
+    printf("%d",values[0]);
+}
+
+int main(int argc, char **argv){
+
+    double avg_time_taken;
+    double time_taken;
+    int delay_value = 0; // in useconds
+    long int loops = 10000000000;
+
+    srand((unsigned)time(NULL));
+    float x_value = 1000 * (float)rand()/RAND_MAX;
+    float y_value = 1000 * (float)rand()/RAND_MAX;
+
+    int key;
+    while ((key = getopt (argc, argv, "hd:l:x:y:")) != -1)
+    switch (key){
+      case 'h':
+        print_help();
+        return 0;
+      case 'd':
+        delay_value = atoi(optarg);
+        break;
+      case 'l':
+        loops = atoi(optarg);
+        break;
+      case 'x':
+        x_value = atof(optarg);
+        break;
+      case 'y':
+        y_value = atof(optarg);
+        break;
+      case '?':
+        if (optopt == 'd' || optopt == 'l')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return -1;
+    }
+
+
+    // initialize arrays
+    //fill_arrays_floats(&b[0],&c[0],x_value,y_value);
+
+    struct timespec start, stop;
+    double accum;
+    clock_gettime( CLOCK_REALTIME, &start);
+
+    for (int x=0; x<loops; x++){
+        foo();
+        if (delay_value){
+            usleep(delay_value);
+        }
+    }
+    clock_gettime( CLOCK_REALTIME, &stop);
+
+    accum = ( stop.tv_sec - start.tv_sec )
+          + ( stop.tv_nsec - start.tv_nsec )
+            / BILLION;
+    
+    avg_time_taken =(accum)/loops;
+
+    //if (check_arrays_float(x_value + y_value,&a[0]))
+    //    return -1;
+    //print_result(loops,delay_value,accum,avg_time_taken,
+    //        x_value,
+    //        y_value,
+    //        x_value+y_value);
+    return 0;
+}
