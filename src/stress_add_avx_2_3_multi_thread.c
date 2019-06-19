95618b203234e530dcbf05066da815ac0229763f
VictorRodriguez <vm.rod25@gmail.com>
2019-06-19 10:09:57 -0500

    better aproach with checker
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- src/stress_add_avx_2_3_multi_thread.c
+++ src/stress_add_avx_2_3_multi_thread.c
@@ -6,11 +6,11 @@
 #include <immintrin.h>
 #include <pthread.h>
 
-// slow fail
-#define LOOPS 1000000000000
+// slow loop
+//#define LOOPS 1000000000000
 
-// fast fail
-// #define LOOPS 1000000000
+// fast loop
+#define LOOPS 1000000
 #define THREADS 48
 
 float a_256[256] = {0};
@@ -19,12 +19,18 @@ float b[256] = {0};
 float c[256] = {0};
 
 
-void fill_arrays_floats(float *b, float *c,
-        float random_1, float random_2){
+void fill_arrays_floats(float random_1, float random_2){
     for (int i=0; i<256; i++){
         b[i] = random_1;
         c[i] = random_2;
     }
+/*
+    for (int i=0; i<256; i++){
+        printf("b = %f\n",b[i]);
+        printf("c = %f\n",c[i]);
+    }
+*/
+
 }
 
 void foo_256(){
@@ -37,25 +43,24 @@ void foo_256(){
         }
 }
 
-void foo_512(){
-
-    //do operation
-	__m512 result,B,C;
-        for (int i=0; i<256; i+=16){
-            B =  _mm512_load_ps(&b[i]);
-            C =  _mm512_load_ps(&c[i]);
-            result = _mm512_add_ps(B,C);
-            _mm512_store_ps(&a_512[i], result);
-        }
-
-}
+//void foo_512(){
+//
+//    //do operation
+//	__m512 result,B,C;
+//        for (int i=0; i<256; i+=16){
+//            B =  _mm512_load_ps(&b[i]);
+//            C =  _mm512_load_ps(&c[i]);
+//            result = _mm512_add_ps(B,C);
+//            _mm512_store_ps(&a_512[i], result);
+//        }
+//
+//}
 
 void *ThreadFoo(void *vargp) {
-
-        for (int x=0; x<LOOPS; x++){
-		foo_256();
-        foo_512();
-        }
+    for (int x=0; x<LOOPS; x++){
+        foo_256();
+        //foo_512();
+    }
 }
 
 int main(int argc, char **argv){
@@ -63,18 +68,28 @@ int main(int argc, char **argv){
     float x_value = 1000 * (float)rand()/RAND_MAX;
     float y_value = 1000 * (float)rand()/RAND_MAX;
 
-    // initialize arrays
-    fill_arrays_floats(&b[0],&c[0],x_value,y_value);
+    float result = x_value + y_value;
 
-    int i;
-    pthread_t tid;
-    for (i = 0; i < THREADS; i++){
-        pthread_create(&tid, NULL, ThreadFoo,NULL);
+    // initialize arrays
+    fill_arrays_floats(x_value,y_value);
+
+    printf("Expected result = %f\n" , result);
+    pthread_t tid[THREADS];
+    for (int i = 0; i < THREADS; i++){
+        pthread_create(&(tid[i]), NULL, ThreadFoo,NULL);
 	}
 
-//	sleep(10);
-//	__asm__("ud2");
-    pthread_exit(NULL);
+    for (int i = 0; i < THREADS; i++){
+        pthread_join(tid[i], NULL);
+    }
+
+    for (int i=0; i<256; i++){
+        if (a_256[i] != result){
+            printf("Error in element %d\n",i);
+            return -1;
+        }
+    }
+    printf("Test PASS\n");
 
 }
 
