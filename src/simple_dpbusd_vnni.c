2bd459ce76338cfb0021efeeb6b6ed555a2d33df
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2019-10-29 18:42:06 +0000

    add check point of working code
    Numerical: 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
    Numerical: 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
    Numerical: 1 1 1 1
    Numerical: 61 61 61 61
    
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- src/simple_dpbusd_vnni.c
+++ src/simple_dpbusd_vnni.c
@@ -4,72 +4,64 @@
 #include <stdint.h>
 
 #define MAXRAND 100
-#define N 8
-
-//int32_t result[N];
+#define N 16
 
 
-void print128_num(__m128i var)
+void print128_num_32(__m128i var)
+{
+    uint32_t *val = (uint32_t*) &var;
+    printf("Numerical: %i %i %i %i \n",
+           val[0], val[1], val[2], val[3]);
+}
+void print128_num_16(__m128i var)
 {
     uint16_t *val = (uint16_t*) &var;
     printf("Numerical: %i %i %i %i %i %i %i %i \n",
            val[0], val[1], val[2], val[3], val[4], val[5],
-           val[6], val[7]);
+           val[6], val[8]);
+}
+
+void print128_num_8(__m128i var)
+{
+    uint8_t *val = (uint8_t*) &var;
+    printf("Numerical: %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i \n",
+           val[0], val[1], val[2], val[3], val[4], val[5],
+           val[6], val[7], val[8], val[9], val[10], val[11],
+           val[12], val[13], val[14], val[15]);
 }
 
 void foo(){
 
-	uint16_t arr_a[N];
-	uint16_t arr_b[N];
+	uint8_t arr_a[N];
+	uint8_t arr_b[N];
+	uint32_t arr_src[4] = {1,1,1,1};
 
     for (int i=0; i<N; i++){
-        arr_a[i] = 2;
-        arr_b[i] = 3;
-    }
+        arr_a[i] = 3;
+        arr_b[i] = 5;
+	}
 
-    __m128i A,B,result;
+    __m128i A,B,src,result;
 
 	A = _mm_loadu_si128((__m128i*)&arr_a);
-	print128_num(A);
+	print128_num_8(A);
 
 	B = _mm_loadu_si128((__m128i*)&arr_b);
-	print128_num(B);
+	print128_num_8(B);
 
-	result = _mm_add_epi32(A,B);
-	print128_num(result);
+	src = _mm_loadu_si128((__m128i*)&arr_src);
+	print128_num_32(src);
+
+	//result = _mm_add_epi16(A,B);
+	//result = _mm_maddubs_epi16(A,B);
+	result =  _mm_dpbusds_epi32(src,A,B);
+	print128_num_32(result);
 
 }
 
 int main(int argc, char **argv){
 
-
-//    int x_value = (rand() % MAXRAND) + 1;
-//    int y_value = (rand() % MAXRAND) + 1;
-//    int z_value = (rand() % MAXRAND) + 1;
-
-//    int x_value = 0;
-//    int y_value = 1;
-//    int z_value = 0;
-
-//    int expected_res = 0;
-//	expected_res = ((x_value*y_value) + z_value);
-
-//    printf("x = %d\n",x_value);
-//    printf("y = %d\n",y_value);
-//    printf("z = %d\n",z_value);
-
-
     foo();
-
-//    for (int i=0; i<N; i++){
-//		printf("%d ",result[i]);
-//		if(result[i] != expected_res){
-//			printf("Arithmetic Error\n");
-//			return -1;
-//		}
-//	}
-//	printf("VPDPBUSD works correctly!\n");
-//	printf("Result: %d\n",expected_res);
 
     return 0;
 }
