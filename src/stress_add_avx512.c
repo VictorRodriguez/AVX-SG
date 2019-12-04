476e1b6d4a35a91fcf532f3e1e49c7bb907cb7de
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2019-12-04 17:48:05 +0000

    make simple parameters_handler for avx 512
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- src/stress_add_avx512.c
+++ src/stress_add_avx512.c
@@ -24,56 +24,20 @@ void foo(){
 
 int main(int argc, char **argv){
 
-    double avg_time_taken;
-    int delay_value = 0; // in useconds
-    long int loops = 10000000000;
-
-    srand((unsigned)time(NULL));
-    float x_value = 1000 * (float)rand()/RAND_MAX;
-    float y_value = 1000 * (float)rand()/RAND_MAX;
-
-    int key;
-    while ((key = getopt (argc, argv, "hd:l:x:y:")) != -1)
-    switch (key){
-      case 'h':
-        print_help();
-        return 0;
-      case 'd':
-        delay_value = atoi(optarg);
-        break;
-      case 'l':
-        loops = atoi(optarg);
-        break;
-      case 'x':
-        x_value = atof(optarg);
-        break;
-      case 'y':
-        y_value = atof(optarg);
-        break;
-      case '?':
-        if (optopt == 'd' || optopt == 'l')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return -1;
-    }
-
+	struct test_parameters param;
+	param = parameters_handler(argc,argv);
 
     // initialize arrays
-    fill_arrays_floats(&b[0],&c[0],x_value,y_value);
+    fill_arrays_floats(&b[0],&c[0],param.x_value,param.y_value);
 
     struct timespec start, stop;
     double accum;
     clock_gettime( CLOCK_REALTIME, &start);
 
-    for (int x=0; x<loops; x++){
+    for (int x=0; x<param.loops; x++){
         foo();
-        if (delay_value){
-            usleep(delay_value);
+        if (param.delay_value){
+            usleep(param.delay_value);
         }
     }
     clock_gettime( CLOCK_REALTIME, &stop);
@@ -81,14 +45,14 @@ int main(int argc, char **argv){
     accum = ( stop.tv_sec - start.tv_sec )
           + ( stop.tv_nsec - start.tv_nsec )
             / BILLION;
-    
-    avg_time_taken =(accum)/loops;
 
-    if (check_arrays_float(x_value + y_value,&a[0]))
+    param.avg_time_taken =(accum)/param.loops;
+
+    if (check_arrays_float(param.x_value + param.y_value,&a[0]))
         return -1;
-    print_result(loops,delay_value,accum,avg_time_taken,
-            x_value,
-            y_value,
-            x_value+y_value);
+    print_result(param.loops,param.delay_value,accum,param.avg_time_taken,
+            param.x_value,
+            param.y_value,
+            param.x_value + param.y_value);
     return 0;
 }
