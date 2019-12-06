a73214936aa8169e04b618311bffa8d3ddb4c936
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2019-12-06 18:09:47 +0000

    last clean up ofoptions menu
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- src/stress_add_d_avx512.c
+++ src/stress_add_d_avx512.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <immintrin.h>
+#include "common.h"
 
 #define BILLION 1E9
 
@@ -13,102 +14,48 @@ float c[256] = {0};
 
 void foo(){
     __m512 result,B,C;
-        for (int i=0; i<256; i+=16){
-            B =  _mm512_load_ps(&b[i]);
-            C =  _mm512_load_ps(&c[i]);
-            result = _mm512_add_ps(B,C);
-            _mm512_store_ps(&a[i], result);
-        }
+	for (int i=0; i<sizeof(a)/sizeof(a[0]); i+=sizeof(__m512)/sizeof(float)){
+		B =  _mm512_load_ps(&b[i]);
+		C =  _mm512_load_ps(&c[i]);
+		result = _mm512_add_ps(B,C);
+		_mm512_store_ps(&a[i], result);
+	}
 }
 
-void fill_arrays(){
-    for (int i=0; i<256; i++){
-        b[i] = 1.0;
-        c[i] = 2.0;
-
-    }
-}
-
-int check_arrays(){
-    int ret = 0;
-    for (int i=0; i<256; i++){
-        if (a[i] == 3)
-            continue;
-        else
-            printf("FAIL, corruption in arithmetic");
-            ret =  -1;
-            break;
-    }
-    return ret;
-}
-
-void print_help(){
-    printf("-h : Help\n");
-    printf("-d <delay> : Delay in useconds\n");
-    printf("-l <delay> : Loops\n");
-}
 
 int main(int argc, char **argv){
 
-    double avg_time_taken;
+	struct test_parameters param;
+	param = parameters_handler(argc,argv);
 
-    // initialize arrays
-    fill_arrays();
-
-    int delay_value = 0; // in useconds
-    long int loops = 10000000000;
-    int option;
-
-    opterr = 0;
-    while ((option = getopt (argc, argv, "hd:l:")) != -1)
-    switch (option){
-      case 'h':
-        print_help();
-        return 0;
-      case 'd':
-        delay_value = atoi(optarg);
-        break;
-      case 'l':
-        loops = atoi(optarg);
-        break;
-      case '?':
-        if (optopt == 'd' || optopt == 'l')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return -1;
-    }
-
+    fill_array_float(&b[0],sizeof(b)/sizeof(b[0]),param.x_value);
+    fill_array_float(&c[0],sizeof(c)/sizeof(c[0]),param.y_value);
 
     struct timespec start, stop;
     double accum;
 
-    clock_gettime( CLOCK_REALTIME, &start);
-    for (int x=0; x<loops; x++){
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    for (int x=0; x<param.loops; x++){
         foo();
-        if (delay_value){
-            usleep(delay_value);
+        if (param.delay_value){
+            usleep(param.delay_value);
         }
     }
+
     clock_gettime( CLOCK_REALTIME, &stop);
 
     accum = ( stop.tv_sec - start.tv_sec )
           + ( stop.tv_nsec - start.tv_nsec )
             / BILLION;
 
-    avg_time_taken =(accum) /loops;
+    param.avg_time_taken =(accum)/param.loops;
 
-    if (check_arrays())
+    if (check_arrays_float(param.x_value + param.y_value,&a[0]))
         return -1;
-
-
-    printf("Loops: %ld\n",loops);
-    printf("Delay per function: %.9g in seconds \n",(delay_value/1E6));
-    printf("Total time: %.9g seconds to execute \n",accum);
-    printf("foo() took %.9g seconds in avg to execute \n", avg_time_taken );
+    print_result(param.loops,param.delay_value,accum,param.avg_time_taken,
+            param.x_value,
+            param.y_value,
+            param.x_value + param.y_value);
     return 0;
 }
